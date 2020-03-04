4d4078261682958f1546fdf1df34642af45a03e7
christoph.karlen@gmail.com <christoph.karlen@gmail.com>
2020-03-04 14:01:08 +0100

    Working on products unittest

--- tests/Unit/ProductsTest.php
+++ tests/Unit/ProductsTest.php
@@ -2,23 +2,38 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Facades\DB;
 use PHPUnit\Framework\TestCase;
 use App\Product;
 
 class ProductsTest extends TestCase
 {
+
+//    protected function setUp(): void
+//    {
+//        parent::setUp();
+//        DB::beginTransaction();
+//    }
+
     /**
      * @return void
      */
     public function test_add_product_to_db()
     {
-		$newProduct = new Product();
-		$newProduct->name = "test";
-		$newProduct->price = 10.20;
-		$newProduct->save();
-		
-		$storedProduct = Product::where('name',$product->name)->first();
-        $this->assertEquals($newProduct->price,$storedProduct->price);
-		$storedProduct->delete();
+        $this->assertTrue(false);
+//		$newProduct = new Product();
+//		$newProduct->name = "test";
+//		$newProduct->price = 10.20;
+//		$newProduct->save();
+
+//		$storedProduct = Product::where('name',$newProduct->name)->first();
+//        $this->assertEquals($newProduct->price,$storedProduct->price);
+//		$storedProduct->delete();
     }
+
+//    public function tearDown() : void
+//    {
+//        DB::rollBack();
+//        parent::tearDown();
+//    }
 }
