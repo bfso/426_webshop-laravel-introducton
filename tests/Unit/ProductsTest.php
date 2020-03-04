29eb983b9a9408eb3f6cef7b39db46945216458e
christoph.karlen@gmail.com <christoph.karlen@gmail.com>
2020-03-04 17:26:40 +0100

    Refactor unittest products

--- tests/Unit/ProductsTest.php
+++ tests/Unit/ProductsTest.php
@@ -1,39 +1,47 @@
 <?php
 
 namespace Tests\Unit;
-
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
 use PHPUnit\Framework\TestCase;
 use App\Product;
+use Tests\CreatesApplication;
 
 class ProductsTest extends TestCase
 {
+    use CreatesApplication;
+    use WithFaker;
 
-//    protected function setUp(): void
-//    {
-//        parent::setUp();
-//        DB::beginTransaction();
-//    }
+    protected $app;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->app = $this->createApplication();
+        $this->setUpFaker();
+        //$this->app->make('Illuminate\Database\Eloquent\Factory')->load(__DIR__ . '/../../database/factories');
+        DB::beginTransaction();
+    }
 
     /**
      * @return void
      */
     public function test_add_product_to_db()
     {
-        $this->assertTrue(false);
-//		$newProduct = new Product();
-//		$newProduct->name = "test";
-//		$newProduct->price = 10.20;
-//		$newProduct->save();
+		$newProduct = new Product();
+		$newProduct->name = $this->faker->word;
+		$newProduct->price = $this->faker->randomFloat(2,-10,100);
+		$newProduct->save();
 
-//		$storedProduct = Product::where('name',$newProduct->name)->first();
-//        $this->assertEquals($newProduct->price,$storedProduct->price);
-//		$storedProduct->delete();
+		$storedProduct = Product::where('id',$newProduct->id)->first();
+        $this->assertEquals($newProduct->name,$storedProduct->name);
+        $this->assertEquals($newProduct->price,$storedProduct->price);
     }
 
-//    public function tearDown() : void
-//    {
-//        DB::rollBack();
-//        parent::tearDown();
-//    }
+    public function tearDown() : void
+    {
+        DB::rollBack();
+        parent::tearDown();
+    }
 }
